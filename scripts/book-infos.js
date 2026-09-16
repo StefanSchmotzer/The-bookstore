4020593edfe9ac7ef644802599c7432445393b8c
Stefan Schmotzer <s-schmotzer@t-online.de>
2026-09-16 09:08:40 +0200

    add book infos

---
+++ scripts/book-infos.js
@@ -0,0 +1,109 @@
+let books = [
+    {
+      "name":  "Buch 1",
+      "author": "Maxine Mustermann",
+      "gender": "female",
+      "likes": 15,
+      "liked": true,
+      "price": 1.99,
+      "publishedYear": 2018,
+      "genre": "Fantasy",
+      "comments": [
+        {
+          "name": "Leser123",
+          "comment": "dsdeafdach, asfsdaf sgsdv sdvefesselt hat."
+        },
+        {
+          "name": "Bookworm84",
+          "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+        },
+        {
+          "name": "FantasyFanatic",
+          "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+        },
+        {
+          "name": "SciFiGuru",
+          "comment": "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren."
+        },
+        {
+          "name": "NovelLover",
+          "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+        }
+      ]
+    },
+    {
+      "name": "Buch 2",
+      "author": "Lorenz Büffel",
+      "gender": "male",
+      "likes": 80,
+      "liked": false,
+      "price": 4.50,
+      "publishedYear": 2021,
+      "genre": "Fantasy",
+      "comments": []
+    },
+    {
+      "name": "Der Horizont der Liebe",
+      "gender": "female",
+      "author": "Florentine Ferrant",
+      "likes": 3480,
+      "liked": true,
+      "price": 22.95,
+      "publishedYear": 2019,
+      "genre": "Romantik",
+      "comments": [
+        {
+          "name": "LeserPeter",
+          "comment": "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt."
+        },
+        {
+          "name": "BookLover21",
+          "comment": "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat."
+        },
+        {
+          "name": "FantasyNerd",
+          "comment": "Fantastische Welten und epische Abenteuer - genau mein Geschmack!"
+        },
+        {
+          "name": "SciFiEnthusiast",
+          "comment": "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht."
+        },
+        {
+          "name": "ReadingAddict",
+          "comment": "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat."
+        }
+      ]
+    },
+    {
+      "name": "Der Hund, die Katze und die Maus",
+      "author": "Tomm und Jerry",
+      "gender": "male",
+      "likes": 750,
+      "liked": false,
+      "price": 18.00,
+      "publishedYear": 2020,
+      "genre": "Abenteuer",
+      "comments": [
+        {
+          "name": "Käsefreund",
+          "comment": "Ein spannendes Abenteuer, auch wenn die Katze zu wenig Schmerz erleiden muss."
+        },
+        {
+          "name": "LeseWurm",
+          "comment": "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben."
+        }
+      ]
+    },
+    {
+      "name": "Die Macht der Sprache",
+      "author": "Sibille Hortensie",
+      "gender": "female",
+      "likes": 1300,
+      "liked": true,
+      "price": 16.75,
+      "publishedYear": 2017,
+      "genre": "Fantasy",
+      "comments": []
+    },
+
+  ]
