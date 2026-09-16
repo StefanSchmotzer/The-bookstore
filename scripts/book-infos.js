af5e54519d9637ee16628e0fa1b202d4ebfb9a05
Stefan Schmotzer <s-schmotzer@t-online.de>
2026-09-16 16:12:41 +0200

    last style before @media

--- scripts/book-infos.js
+++ scripts/book-infos.js
@@ -1,107 +1,111 @@
 let books = [
     {
-      "name":  "Buch 1",
+      "name":  "Das Leben eines besonderen Bullis",
       "imagePath": "./assets/img/booktunnel.webp",
-      "author": "Maxine Mustermann",
+      "author": "Serafine Semmelbrösel",
       "gender": "female",
       "likes": 15,
       "liked": true,
-      "price": 1.99,
-      "publishedYear": 2018,
+      "price": 23.99,
+      "publishedYear": 2024,
       "genre": "Fantasy",
       "comments": [
         {
-          "name": "Leser123",
-          "comment": "dsdeafdach, asfsdaf sgsdv sdvefesselt hat."
+          "name": "Der Büchergott",
+          "comment": "Liebevolle Figuren, magische Momente und eine wunderschöne Botschaft über Mut und Freundschaft."
         },
         {
-          "name": "Bookworm84",
-          "comment": "Eine romantische Geschichte, die mein Herz berührt und mich zum Nachdenken gebracht hat."
+          "name": "Der entfesselte Leser",
+          "comment": "Dieses Buch entführt auf charmante Weise in eine fantasievolle Welt,perfekt für große und kleine Leser."
         },
         {
-          "name": "FantasyFanatic",
-          "comment": "Eine spannende Fantasiewelt, die ich nur schwer aus der Hand legen konnte."
+          "name": "Ludwig Lesezirkel",
+          "comment": "Herzerwärmend, originell und wunderbar erzählt ein Fantasybuch, das man gerne weiterempfiehlt."
         },
         {
-          "name": "SciFiGuru",
-          "comment": "Ein cleverer Science-Fiction-Roman mit interessanten Zeitreise-Konzepten und Charakteren."
+          "name": "RetroReader",
+          "comment": "Eine herrlich originelle Idee! Die Charaktere sind liebenswert und die Handlung bleibt spannend, ohne den leichten Ton zu verlieren. Ein absoluter Geheimtipp für alle, die etwas Neues suchen."
         },
         {
-          "name": "NovelLover",
-          "comment": "Ein Buch, das voller magischer Überraschungen steckt und mich begeistert hat."
+          "name": "Der Lesefan",
+          "comment": "Beim Titel dachte ich erst, Oh Gott eine Neuauflage von 'Herbie ein Tollfer Käfer' oder 'DuDu' aber es ist mal ein ganz andere Sichtweise"
         }
       ]
     },
     {
-      "name": "Buch 2",
-      "imagePath": "./assets/img/books/best-seller.png",
-      "author": "Lorenz Büffel",
+      "name": "Von der Schule ins Universum",
+      "imagePath": "./assets/img/books/",
+      "author": "Ronny Rabenschwarz",
       "gender": "male",
       "likes": 80,
       "liked": false,
-      "price": 4.50,
-      "publishedYear": 2021,
+      "price": 14.50,
+      "publishedYear": 2026,
       "genre": "Fantasy",
       "comments": []
     },
     {
-      "name": "Der Horizont der Liebe",
+      "name": "Liebe endet nicht am Horizont",
       "imagePath": "./assets/img/books/best-seller.png",
       "gender": "female",
       "author": "Florentine Ferrant",
-      "likes": 3480,
+      "likes": 3427,
       "liked": true,
-      "price": 22.95,
-      "publishedYear": 2019,
+      "price": 31.95,
+      "publishedYear": 2021,
       "genre": "Romantik",
       "comments": [
         {
-          "name": "LeserPeter",
-          "comment": "Die Handlung war fesselnd und die Charaktere unglaublich lebendig dargestellt."
+          "name": "Liebesengel-81",
+          "comment": "Wunderschön emotional geschrieben. Während des Lesens sind mehrfach Tränen auf die Seiten getropft"
         },
         {
-          "name": "BookLover21",
-          "comment": "Ein romantisches Meisterwerk, das mich tief berührt und bewegt hat."
+          "name": "vergesseneSeele69",
+          "comment": "An Romantik kaum zu überbieten. Ich habe es schon viermal gelesen und noch immer fesselt es mich."
         },
         {
-          "name": "FantasyNerd",
-          "comment": "Fantastische Welten und epische Abenteuer - genau mein Geschmack!"
+          "name": "Das unzerbrechliche Herz",
+          "comment": "Leider hat es nichts mit der Realität gemeinsam. Ich warte noch immer auf ein Zeichen von José de la Cruz."
         },
         {
-          "name": "SciFiEnthusiast",
-          "comment": "Die Zeitreise-Elemente waren genial und haben die Story spannend gemacht."
+          "name": "SciFiExperte",
+          "comment": "Ich dachte es geht um die Liebe zum Fliegen. Es ist aber ganz gut geschrieben."
         },
         {
-          "name": "ReadingAddict",
-          "comment": "Ein unvergessliches Buch, das mich auf eine magische Reise mitgenommen hat."
+          "name": "JimmyHoliday",
+          "comment": "Ein unvergessliches Buch, das mir geholfen hat meine Gefühle zu entdecken."
         }
       ]
     },
     {
-      "name": "Der Hund, die Katze und die Maus",
+      "name": "Agent Smith entdeckt die Welt",
       "imagePath": "./assets/img/books/best-seller.png",
-      "author": "Tomm und Jerry",
+      "author": "Thomas A. Anderson",
       "gender": "male",
       "likes": 750,
       "liked": false,
-      "price": 18.00,
-      "publishedYear": 2020,
+      "price": 46.30,
+      "publishedYear": 2025,
       "genre": "Abenteuer",
       "comments": [
         {
-          "name": "Käsefreund",
-          "comment": "Ein spannendes Abenteuer, auch wenn die Katze zu wenig Schmerz erleiden muss."
+          "name": "Trinity",
+          "comment": "Eine ganz neue Sichtweise, welche den Zwiespalt und das Übermaß an Emotionen der Hauptrolle nahezu perfekt wiederspeigelt"
         },
         {
-          "name": "LeseWurm",
-          "comment": "Die Liebesgeschichte war herzergreifend und wunderschön geschrieben."
+          "name": "The Codingmaster",
+          "comment": "Durch das Buch sehe ich die legendären Filme mit ganz anderen Augen."
+        },
+        {
+          "name": "Ricarda Jobless",
+          "comment": "Wer in der IT Welt zu Hause ist, wird sich in diesem Buch perfekt aufgehoben fühlen."
         }
       ]
     },
     {
-      "name": "Die Macht der Sprache",
+      "name": "Wenn Worte stärker sind als jede Waffe",
       "imagePath": "./assets/img/books/best-seller.png",
-      "author": "Sibille Hortensie",
+      "author": "Sibille Sonnenlicht",
       "gender": "female",
       "likes": 1300,
       "liked": true,
